e05555b2400c8b8aa5f25087a3ebd219a1182f89
kellydunn <defaultstring@gmail.com>
2015-03-06 11:36:16 -0800

    Sound is moving

--- drum.go
+++ drum.go
@@ -5,7 +5,7 @@ package drum
 import (
 	"code.google.com/p/portaudio-go/portaudio"
 	"github.com/mkb218/gosndfile/sndfile"
-        "time"
+	"time"
 )
 
 const (
@@ -24,27 +24,31 @@ func LoadSample(filename string) (*Sample, error) {
 	}
 
 	defer soundFile.Close()
-	buffer := make([]float32, 10*info.Samplerate*info.Channels)        
+	buffer := make([]float32, 8192)
 	s := &Sample{
 		Buffer: buffer,
 	}
 
-	s.Stream, err = portaudio.OpenDefaultStream(0, 1, 44100, len(s.Buffer), &s.Buffer)
+	s.Stream, err = portaudio.OpenDefaultStream(0, 2, 44100, 256, &s.Buffer)
 	if err != nil {
 		return nil, err
 	}
 
-        s.Stream.Start()
-        defer s.Stream.Close()
+	s.Stream.Start()
+	defer s.Stream.Close()
 
 	_, err = soundFile.ReadItems(buffer)
 	if err != nil {
 		return nil, err
 	}
 
-        time.Sleep(time.Second * 10)
+	s.Stream.Write()
 
-        return s, nil
+        time.Sleep(time.Second * 5)
+
+	s.Stream.Stop()
+
+	return s, nil
 }
 
 func (s *Sample) Play(in []float32, out []float32) {
@@ -54,6 +58,6 @@ func (s *Sample) Play(in []float32, out []float32) {
 }
 
 type Sample struct {
-	Buffer    []float32
+	Buffer []float32
 	Stream *portaudio.Stream
 }
